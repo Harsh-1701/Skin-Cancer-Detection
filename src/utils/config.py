d477b5e6747f645d2ddb41cc2cf4496ba288ce7e
Harsh-1701 <imhrs1714@gmail.com>
2026-07-31 22:28:21 +0530

    Added configuration module and exploratory data analysis

---
+++ src/utils/config.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+# Project Root
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+
+# Dataset
+DATASET_DIR = PROJECT_ROOT / "dataset"
+
+IMAGE_DIRS = [
+    DATASET_DIR / "images" / "HAM10000_images_part_1",
+    DATASET_DIR / "images" / "HAM10000_images_part_2",
+]
+
+METADATA_PATH = (
+    DATASET_DIR
+    / "metadata"
+    / "HAM10000_metadata.csv"
+)
+
+# Outputs
+OUTPUT_DIR = PROJECT_ROOT / "outputs"
+
+MODEL_DIR = OUTPUT_DIR / "models"
+PLOT_DIR = OUTPUT_DIR / "plots"
+LOG_DIR = OUTPUT_DIR / "logs"
+PREDICTION_DIR = OUTPUT_DIR / "predictions"
+
+# Image Size
+IMAGE_SIZE = (224, 224)
+
+# Random Seed
+RANDOM_SEED = 42
